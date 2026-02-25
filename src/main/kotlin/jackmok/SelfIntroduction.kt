c0c36471f035da0ad381ddb65b129a16f4cc0c04
jackmok0829 <jackmokchunghiu@gmail.com>
2026-02-25 17:05:24 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/jackmok/SelfIntroduction.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.jackmok
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is Mok Chung Hiu.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
